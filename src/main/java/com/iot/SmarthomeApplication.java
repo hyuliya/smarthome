6a36af736192bcb6f49c4a72f07770590544dbf9
Hyuliya Hasan <hyuliyahasan00@gmail.com>
2017-03-20 15:10:01 +0200

    Initial commit

---
+++ src/main/java/com/iot/SmarthomeApplication.java
@@ -0,0 +1,12 @@
+package com.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthomeApplication.class, args);
+	}
+}
